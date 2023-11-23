28afd2fa79a14a4027e2a621d78e8be68101c497
rokadesonali9322 <rokadesonali9322@gmail.com>
2023-11-23 22:45:37 +0530

    first commit

---
+++ src/components/Home/MainHomeComp.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Home from './Home'
+import Cards from './Cards'
+import Footer from './Footer'
+
+const MainHomeComp = () => {
+  return (
+    <>
+      {/* <Header /> */}
+      <Home />
+      <Cards />
+      <Footer />
+    </>
+  )
+}
+
+export default MainHomeComp
